4fcbefd44f01fc0136342156f65cfb4e28a4ea36
Zala <zala.developer@gmail.com>
2024-07-28 19:14:18 +0930

    configure localstorage with persist middleware

--- src/store/items-store.js
+++ src/store/items-store.js
@@ -1,63 +1,60 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-// Function to get items from localStorage
-const getInitialItems = () => {
-  const items = localStorage.getItem("items");
-  return items ? JSON.parse(items) : [];
-};
-
-export const useItemsStore = create((set) => ({
-  items: getInitialItems(),
-  addItem: (newItemName) => {
-    set((state) => {
-      const newItem = {
-        id: new Date().getTime(),
-        name: newItemName,
-        packed: false,
-      };
-      const newItems = [...state.items, newItem];
-      return { items: newItems };
-    });
-  },
-  deleteItem: (id) => {
-    set((state) => {
-      const newItems = state.items.filter((item) => item.id !== id);
-      return { items: newItems };
-    });
-  },
-  removeAllItems: () => {
-    set(() => ({ items: [] }));
-  },
-  toggleCheckbox: (id) => {
-    set((state) => {
-      const newItems = state.items.map((item) => {
-        if (item.id === id) {
-          return { ...item, packed: !item.packed };
-        }
-        return item;
-      });
-      return { items: newItems };
-    });
-  },
-  markAllAsComplete: () => {
-    set((state) => {
-      const newItems = state.items.map((item) => {
-        return { ...item, packed: true };
-      });
-      return { items: newItems };
-    });
-  },
-  markAllAsIncomplete: () => {
-    set((state) => {
-      const newItems = state.items.map((item) => {
-        return { ...item, packed: false };
-      });
-      return { items: newItems };
-    });
-  },
-}));
-
-// Subscribe to state changes to update localStorage
-useItemsStore.subscribe((state) => {
-  localStorage.setItem("items", JSON.stringify(state.items));
-});
+export const useItemsStore = create(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (newItemName) => {
+        set((state) => {
+          const newItem = {
+            id: new Date().getTime(),
+            name: newItemName,
+            packed: false,
+          };
+          const newItems = [...state.items, newItem];
+          return { items: newItems };
+        });
+      },
+      deleteItem: (id) => {
+        set((state) => {
+          const newItems = state.items.filter((item) => item.id !== id);
+          return { items: newItems };
+        });
+      },
+      removeAllItems: () => {
+        set(() => ({ items: [] }));
+      },
+      toggleCheckbox: (id) => {
+        set((state) => {
+          const newItems = state.items.map((item) => {
+            if (item.id === id) {
+              return { ...item, packed: !item.packed };
+            }
+            return item;
+          });
+          return { items: newItems };
+        });
+      },
+      markAllAsComplete: () => {
+        set((state) => {
+          const newItems = state.items.map((item) => {
+            return { ...item, packed: true };
+          });
+          return { items: newItems };
+        });
+      },
+      markAllAsIncomplete: () => {
+        set((state) => {
+          const newItems = state.items.map((item) => {
+            return { ...item, packed: false };
+          });
+          return { items: newItems };
+        });
+      },
+    }),
+    {
+      name: "items",
+    }
+  )
+);
